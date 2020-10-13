563cc96b3d458def7d3d67207dcbc0cd2d2ab8b0
Alex Kislukhin <alexandr.kislukhin@gmail.com>
2020-10-13 04:57:59 +0300

    mobile breadcrumbs

---
+++ src/components/Breadcrumbs/mobile.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Breadcrumbs.scss";
+
+type Props = {
+    path: { url: string; name: string }[];
+};
+
+export const BreadcrumbsMobile: React.FC<Props> = ({ path }) => {
+    const links = [{ url: "/", name: "HOME" }];
+
+    let url = "";
+
+    for (let i = 0; i < path.length; i++) {
+        url += `/${path[i].url}`;
+        links.push({ name: path[i].name, url });
+    }
+
+    return (
+        <ul className="Maritel__Breadcrumbs hide-mobile">
+            {links.map((item, i) => {
+                if (i === links.length - 1) {
+                    return (
+                        <li key={i} className="Maritel__Breadcrumbs__Active">
+                            {item.name}
+                        </li>
+                    );
+                }
+
+                return (
+                    <li key={i}>
+                        <Link to={item.url}>{item.name}&nbsp;/&nbsp;</Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
